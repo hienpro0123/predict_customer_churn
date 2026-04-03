62093c3dbd079bf8e5cbdd60084ef63eed103569
hienpro0123 <trinhquanghien.iuh@gmail.com>
2026-04-03 11:20:43 +0700

    up

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 import streamlit as st
-
+from db.schema import create_tables
 from ui.tab_predict import (
     render_batch_prediction_tab,
     render_database_prediction_tab,
     render_single_prediction_tab,
 )
 from ui.components import inject_styles, render_header
-
 st.set_page_config(page_title="Customer Churn", layout="wide")
 
 
 def main() -> None:
+    create_tables()
     inject_styles()
     render_header()
     tab_manual, tab_database, tab_batch = st.tabs(
