dce5877215fda153b398fe352c70bb2b3dc27492
hienpro0123 <trinhquanghien.iuh@gmail.com>
2026-04-06 09:23:09 +0700

    p

--- ingest_raw_data.py
+++ ingest_raw_data.py
@@ -4,6 +4,11 @@ RAW_TEST_PATH = "/Volumes/workspace/default/data_customers/raw/customer_churn_da
 BRONZE_TRAIN_PATH = "/Volumes/workspace/default/data_customers/bronze/train"
 BRONZE_TEST_PATH = "/Volumes/workspace/default/data_customers/bronze/test"
 
+def clean_columns(df):
+    for col in df.columns:
+        new_col = col.strip().lower().replace(" ", "_")
+        df = df.withColumnRenamed(col, new_col)
+    return df
 
 df_train = spark.read.csv(
     RAW_TRAIN_PATH,
@@ -16,6 +21,10 @@ df_test = spark.read.csv(
     header=True,
     inferSchema=True,
 )
+
+
+df_train = clean_columns(df_train)
+df_test = clean_columns(df_test)
 
 df_train.write.format("delta").mode("overwrite").save(BRONZE_TRAIN_PATH)
 df_test.write.format("delta").mode("overwrite").save(BRONZE_TEST_PATH)
